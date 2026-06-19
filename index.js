6def25f5d2f65600769f74a683ea489d7f5cafe8
Your Name <your.email@example.com>
2026-06-20 00:22:50 +0600

    created post for donation requests

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-const port = 5000
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require('express');
 const cors = requie('cors')
-
+const app = express()
+const port = 5000
+require('dotenv').config()
 
 app.use(cors())
+const app = express()
+
+const uri = process.env.MONGODB_URI;
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -20,6 +26,16 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const database = client.db("donor-net");
+    const requestCollection = database.collection("requests");
+
+
+    app.post('api/requests', async (req, res) => {
+        const request = req.body;
+        const result = await requestCollection.insertOne(request);
+        res.send(result);
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
